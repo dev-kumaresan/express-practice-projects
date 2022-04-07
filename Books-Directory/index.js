035f9599a73c87352be36ae5dd3dc805ed1257df
dev-kumaresan <dev.kumaresanl@gmail.com>
2022-04-07 18:37:07 +0530

    api-created

--- Books-Directory/index.js
+++ Books-Directory/index.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const api = require('./routes/api');
-
 const app = express();
 const PORT = 5000;
-
 app.use(bodyParser.json());
 app.use('/', api);
-
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
