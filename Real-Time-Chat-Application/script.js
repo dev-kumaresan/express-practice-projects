7c009f15abcd0d70b692d3293869746ef799933f
dev-kumaresan <dev.kumaresanl@gmail.com>
2022-04-10 18:41:31 +0530

    initial-commit

--- Real-Time-Chat-Application/script.js
+++ Real-Time-Chat-Application/script.js
@@ -1,5 +1,10 @@
 const socket = io('http://localhost:3000')
 const messageForm = document.getElementById('send-container')
+const messageInput = document.getElementById('user-message')
 socket.on('chat-message',data=>{
     console.log(data)
+})
+messageForm.addEventListener('submit',e => {
+    e.preventDefault()
+    const message = 
 })
