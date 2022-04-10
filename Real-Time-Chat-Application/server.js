5e7680edfbedc23e9230ec45808fe30c93773403
dev-kumaresan <dev.kumaresanl@gmail.com>
2022-04-10 18:59:48 +0530

    initial-commit

--- Real-Time-Chat-Application/server.js
+++ Real-Time-Chat-Application/server.js
@@ -2,7 +2,7 @@ const io = require('socket.io')(3000)
 io.on('connection',socket=>{
     console.log('new User')
   socket.emit('chat message','Hello World');
-  socket.on('send-chat-message',message=>{
-      console.log(message)
+  socket.on('send-chat-message',message => {
+      socket.broadcast.emit('chat-message',message)
   })
-})  
+})
